a558f1a8cb7e5ce13702cf1ff6918ac040673107
philip ross <philip@satchel.scot>
2021-10-21 16:07:43 +0100

    feat: Make auth a class to pass db

--- src/auth.js
+++ src/auth.js
@@ -1,69 +1,74 @@
 import bcrypt from 'bcryptjs';
-import { database } from './models/database.js';
 
 const SALT_WORK_FACTOR_ = 10;
 
-async function authenticate(email, password) {
-  const isEmail = typeof email === 'string' && email.includes('@');
-  // normalize username
-  const conditions = isEmail
-    ? {
-        $or: [{ username: email }, { email }, { lowerEmail: email.toLowerCase() }],
+export class Auth {
+  constructor(database, ObjectId) {
+    this.db = database;
+    this.ObjectId = ObjectId;
+  }
+
+  async authenticate(email, password) {
+    const isEmail = typeof email === 'string' && email.includes('@');
+    // normalize username
+    const conditions = isEmail
+      ? {
+          $or: [
+            { username: email },
+            { email },
+            { lowerEmail: email.toLowerCase() },
+          ],
+        }
+      : { _id: new this.ObjectId(email) };
+
+    const [user, hash] = await this.findUser(conditions);
+
+    if (!user || !hash) {
+      return false;
+    }
+
+    const isMatch = await bcrypt.compare(password, hash);
+
+    return isMatch ? user : false;
+  }
+
+  async findUser(query) {
+    const conditions = {
+      $or: [{ isActive: 'yes' }, { isActive: true }],
+      ...query,
+    };
+
+    if (conditions._id && typeof conditions._id === 'string') {
+      conditions._id = new this.ObjectId(conditions._id);
+    }
+
+    const user = await this.db.users.findOne(conditions, {
+      projection: {
+        _id: 1,
+        username: 1,
+        email: 1,
+        role: 1,
+        groups: 1,
+        name: 1,
+        resources: 1,
+        teams: 1,
+        password: 1,
+      },
+    });
+    if (user) {
+      if (!user.teams) {
+        user.teams = user.resources || [];
       }
-    : { _id: database.toObjectId(email) };
 
-  const [user, hash] = await findUser(conditions);
+      if (!user.role && user.groups) {
+        user.role = user.groups[0] || '';
+      }
+      delete user.resources;
+      delete user.groups;
+      const { password, ...userWithoutPassword } = user;
 
-  if (!user || !hash) {
-    return false;
-  }
-
-  const isMatch = await bcrypt.compare(password, hash);
-
-  return isMatch ? user : false;
-}
-
-async function findUser(query) {
-  const conditions = {
-    $or: [{ isActive: 'yes' }, { isActive: true }],
-    ...query,
-  };
-
-  if (conditions._id && typeof conditions._id === 'string') {
-    conditions._id = database.toObjectId(conditions._id);
-  }
-
-  const user = await database.users.findOne(conditions, {
-    projection: {
-      _id: 1,
-      username: 1,
-      email: 1,
-      role: 1,
-      groups: 1,
-      name: 1,
-      resources: 1,
-      teams: 1,
-      password: 1,
-    },
-  });
-  if (user) {
-    if (!user.teams) {
-      user.teams = user.resources || [];
+      return [userWithoutPassword, password];
     }
-
-    if (!user.role && user.groups) {
-      user.role = user.groups[0] || '';
-    }
-    delete user.resources;
-    delete user.groups;
-    const { password, ...userWithoutPassword } = user;
-
-    return [userWithoutPassword, password];
+    return [];
   }
-  return [];
 }
-
-export default {
-  authenticate,
-  findUser,
-};
