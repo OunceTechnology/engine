5867b96c6d40651122c83eb4e23f848bd8030e11
philip ross <philip@satchel.scot>
2023-08-29 11:30:14 +0100

    choer: layout

--- src/models/database-teams.js
+++ src/models/database-teams.js
@@ -3,7 +3,8 @@ export class Teams {
     this.db = database;
     this.ObjectId = ObjectId;
   }
-  async createTeam({ _id, ...team }) {
+  async createTeam(teamSpec) {
+    const { _id, ...team } = teamSpec;
     const { insertedId } = await this.db.teams.insertOne(team);
 
     return insertedId.toString();
@@ -12,7 +13,10 @@ export class Teams {
   async deleteTeam(teamId) {
     const {
       lastErrorObject: { n },
-    } = await this.db.teams.findOneAndDelete({ _id: new this.ObjectId(teamId) }, { projection: { _id: 1 } });
+    } = await this.db.teams.findOneAndDelete(
+      { _id: new this.ObjectId(teamId) },
+      { projection: { _id: 1 } },
+    );
 
     if (n !== 1) {
       throw new Error('Team not deleted');
@@ -25,7 +29,9 @@ export class Teams {
   }
 
   async getTeamsById(ids) {
-    return this.db.teams.find({ _id: { $in: ids.map(id => new this.ObjectId(id)) } }).toArray();
+    return this.db.teams
+      .find({ _id: { $in: ids.map(id => new this.ObjectId(id)) } })
+      .toArray();
   }
 
   async getTeamByName(name) {
@@ -36,12 +42,17 @@ export class Teams {
     return await this.db.teams.findOne({ _id: new this.ObjectId(teamId) });
   }
 
-  async updateTeam({ _id: id, ...team }) {
+  async updateTeam(teamSpec) {
+    const { _id: id, ...team } = teamSpec;
     const _id = new this.ObjectId(id);
 
     const {
       lastErrorObject: { n },
-    } = await this.db.teams.findOneAndUpdate({ _id }, { $set: team }, { projection: { _id: 1 } });
+    } = await this.db.teams.findOneAndUpdate(
+      { _id },
+      { $set: team },
+      { projection: { _id: 1 } },
+    );
 
     if (n !== 1) {
       throw new Error('Failed updating team');
